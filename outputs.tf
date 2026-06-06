4b247d99b73c8da575bcdeebba176d48737f61b5
Alexander-M-rss <oleksandr.m.rss@gmail.com>
2026-06-06 19:14:25 +0300

    Task 4 is done

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,29 @@
-output "s3_bucket_name" {
-  description = "The name of the S3 bucket."
-  value       = aws_s3_bucket.this.bucket
+output "iam_group_name" {
+  description = "Name of the created IAM group"
+  value       = aws_iam_group.this.name
 }
 
-output "s3_bucket_arn" {
-  description = "The ARN of the S3 bucket."
-  value       = aws_s3_bucket.this.arn
+output "iam_policy_arn" {
+  description = "ARN of the created custom IAM policy"
+  value       = aws_iam_policy.this.arn
+}
+
+output "iam_role_name" {
+  description = "Name of the created IAM role"
+  value       = aws_iam_role.this.name
+}
+
+output "iam_role_arn" {
+  description = "ARN of the created IAM role"
+  value       = aws_iam_role.this.arn
+}
+
+output "iam_instance_profile_name" {
+  description = "Name of the created IAM instance profile"
+  value       = aws_iam_instance_profile.this.name
+}
+
+output "iam_instance_profile_arn" {
+  description = "ARN of the created IAM instance profile"
+  value       = aws_iam_instance_profile.this.arn
 }
--- variables.tf
+++ variables.tf
@@ -1,14 +1,34 @@
 variable "aws_region" {
-  description = "AWS region where resources will be created."
+  description = "AWS region where resources will be created"
   type        = string
 }
 
 variable "project_id" {
-  description = "Unique project identifier used for tagging resources."
+  description = "Project identifier used as the value for the Project tag"
   type        = string
 }
 
 variable "bucket_name" {
-  description = "Globally unique name for the S3 bucket."
+  description = "Name of the pre-created S3 bucket to which the IAM policy will grant write access"
+  type        = string
+}
+
+variable "iam_group_name" {
+  description = "Name of the IAM group to create"
+  type        = string
+}
+
+variable "iam_policy_name" {
+  description = "Name of the custom IAM policy to create"
+  type        = string
+}
+
+variable "iam_role_name" {
+  description = "Name of the IAM role to create for EC2"
+  type        = string
+}
+
+variable "iam_instance_profile_name" {
+  description = "Name of the IAM instance profile to create and associate with the IAM role"
   type        = string
 }
